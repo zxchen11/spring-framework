ca33160776dc7f7edc11b8101569d4bde24cc906
Eddie <2456767505@qq.com>
2020-11-30 23:12:44 +0800

    注释添加

--- src/main/java/top/wlz922/aspect/CustomAspect.java
+++ src/main/java/top/wlz922/aspect/CustomAspect.java
@@ -1,9 +1,8 @@
 package top.wlz922.aspect;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,5 +15,29 @@ public class CustomAspect {
 	@Before("pointCut()")
 	public void before(){
 		log.info("CustomAspect.before execute...");
+	}
+
+
+	@After("pointCut()")
+	public void after(){
+		log.info("CustomAspect.after execute...");
+	}
+
+	@AfterReturning("pointCut()")
+	public void afterReturning() {
+		log.info("CustomAspect.afterReturning execute...");
+	}
+
+	@AfterThrowing("pointCut()")
+	public void afterThrowing() {
+		log.info("CustomAspect.afterThrowing execute...");
+	}
+
+	@Around("pointCut()")
+	public Object around(ProceedingJoinPoint pjp) throws Throwable {
+		log.info("CustomAspect.around before execute...");
+		Object obj = pjp.proceed();
+		log.info("CustomAspect.around after execute...");
+		return obj;
 	}
 }
