332b5124b144a734926320592199d53ae4736bda
Eddie <2456767505@qq.com>
2020-12-10 09:50:35 +0800

    注释完善

--- src/main/java/top/wlz922/SpringSourceMvcAnoApplication.java
+++ src/main/java/top/wlz922/SpringSourceMvcAnoApplication.java
@@ -24,8 +24,7 @@ public class SpringSourceMvcAnoApplication {
 		String userDir = System.getProperty("user.dir");
 		// 这种方式设置System.setProperty("tomcat.util.scan.StandardJarScanFilter.jarsToSkip", "\\,*");
 		// 可以加快启动速度，但是由于读取的字节码文件是maven编译的（maven编译使用的本地仓库的jar包），会导致gradle项目中的代码断点无效。
-		// 这里设置为直接读取src/main/webapp下的文件，自动扫描baseDir下的java代码。（启动慢，可在gradle项目代码中打断点调试。
-		// ）
+		// 这里设置为直接读取src/main/webapp下的文件，自动扫描baseDir下的java代码。（启动慢，可在gradle项目代码中打断点调试。）
 		// String webappDir = userDir + File.separator + "/target/spring-source-mvc-ano-1.0-SNAPSHOT";
 		String webappDir = userDir + File.separator + "/src/main/webapp";
 
