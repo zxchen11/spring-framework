d5566858a26da244f0abb95d73062e7db53ef65a
Eddie <2456767505@qq.com>
2020-12-11 19:52:55 +0800

    修改几行注释

--- src/main/java/top/wlz922/annoboot/MyWebAppInitializer.java
+++ src/main/java/top/wlz922/annoboot/MyWebAppInitializer.java
@@ -6,21 +6,21 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
  * @author Eddie
  */
 public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-	//根容器 父容器
+	/** 根容器 父容器 */
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		logger.info("===========spring根容器加载成功============");
 		return new Class[]{RootConfig.class};
 	}
 
-	//web容器 子容器
+	/** web容器 子容器 */
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		logger.info("===========springweb子容器加载成功============");
 		return new Class[]{WebConfig.class};
 	}
 
-	//获取前端控制器映射信息 dispatcherServlet
+	/** 获取前端控制器映射信息 dispatcherServlet */
 	@Override
 	protected String[] getServletMappings() {
 		return new String[]{"/"};
