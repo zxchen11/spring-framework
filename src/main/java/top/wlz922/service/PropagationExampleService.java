59335bb5887d1f3e848395f9fcca691d3ce9186f
Eddie <2456767505@qq.com>
2020-12-11 18:04:51 +0800

    1

---
+++ src/main/java/top/wlz922/service/PropagationExampleService.java
@@ -0,0 +1,7 @@
+package top.wlz922.service;
+
+public interface PropagationExampleService {
+	void methodA() throws InterruptedException;
+
+	void methodB() throws InterruptedException;
+}
---
+++ src/main/java/top/wlz922/service/impl/PropagationExampleServiceImpl.java
@@ -0,0 +1,26 @@
+package top.wlz922.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import top.wlz922.service.PropagationExampleService;
+
+@Service
+public class PropagationExampleServiceImpl implements PropagationExampleService {
+	@Autowired
+	PropagationExampleService propagationExampleService;
+
+	@Transactional
+	@Override
+	public void methodA() throws InterruptedException {
+		Thread.sleep(100);
+		propagationExampleService.methodB();
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	@Override
+	public void methodB() throws InterruptedException {
+		Thread.sleep(100);
+	}
+}
