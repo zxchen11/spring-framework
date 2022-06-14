887250beccca44c94a6a7f7a4f6224a728a0ad12
zxchen11 <zxchen11@iflytek.com>
2022-06-14 14:02:43 +0800

    提交测试

---
+++ spring-debug/src/main/java/com/zxchen/bean/test/BeanMyTest.java
@@ -0,0 +1,29 @@
+package com.zxchen.bean.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @className:
+ * @description:
+ * @author: zxchen11
+ * @create: 2022-06-14 13:56
+ **/
+public class BeanMyTest {
+
+	private String beanStr = "beanMyTest";
+
+	public String getBeanStr() {
+		return beanStr;
+	}
+
+	public void setBeanStr(String beanStr) {
+		this.beanStr = beanStr;
+	}
+
+	public static void main(String[] args) {
+		BeanFactory bf = new ClassPathXmlApplicationContext("beanTestFactory.xml");
+		BeanMyTest beanMyTest = (BeanMyTest) bf.getBean("beanMyTest");
+		System.out.println("beanStr:" + beanMyTest.getBeanStr());
+	}
+}
